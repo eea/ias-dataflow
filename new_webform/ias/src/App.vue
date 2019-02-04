4a7284a8e3f98bd2295c8d2206cb4fcdfa088b8a
mihai.macaneata <mihai.macaneata@eaudeweb.ro>
2019-02-04 11:43:35 +0200

    integrate bootstrap

--- new_webform/ias/src/App.vue
+++ new_webform/ias/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Webform></Webform>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Webform from './components/Webform.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Webform
   }
 }
 </script>
---
+++ new_webform/ias/src/components/Webform.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="hello">
+		<div><b-btn variant="danger">test</b-btn></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+
+  },
+	created(){
+
+	},
+	computed: {
+
+	},
+	methods: {
+		
+	},
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
