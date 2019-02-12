ec9290bd9da892e8c39f1579df46ffac36133295
mihai-macaneata <themacaneata@gmail.com>
2019-02-13 01:20:07 +0200

    fixed multiselect on ie11

--- webformv2/src/main.js
+++ webformv2/src/main.js
@@ -5,6 +5,7 @@ import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import './css/main.scss'
+import './polyfills.js'
 Vue.use(BootstrapVue);
 
 
